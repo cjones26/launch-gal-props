40b4cdc119181adf85ae285fd29cd1f4eba1c929
C. L. Jones <charlie.l.jones@gmail.com>
2019-04-30 09:23:30 -0400

    Add files via upload
    The main application code utilized to launch the Global Address List (GAL) properties dialog.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace LaunchGALProps
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if(args.Length == 2) {
+                dynamic objApp;
+                dynamic objNamespace;
+
+                objApp = Activator.CreateInstance(Type.GetTypeFromProgID("Outlook.Application"));
+                objNamespace = objApp.GetNamespace("MAPI");
+
+                objNamespace.CreateRecipient(args[1]).AddressEntry.Details();
+            }
+        }
+    }
+}
